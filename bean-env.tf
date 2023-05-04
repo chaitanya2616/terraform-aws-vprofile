2030740ec344231fb798365edad362c5ecdff58e
ChaitanyaDevops <chaitanya.sonawane26@gmail.com>
2023-05-04 13:35:25 +0530

    beanstalk

--- bean-env.tf
+++ bean-env.tf
@@ -1,6 +1,6 @@
-resource "aws_elastic_beanstalk_environment" "vprofile-bean-csprod26" {
+resource "aws_elastic_beanstalk_environment" "vprofile-bean-chaitanyaprod" {
   application         = aws_elastic_beanstalk_application.vprofile-prod26.name
-  name                = "vprofile-bean-prod"
+  name                = "vprofile-bean-chaitanyaprod"
   solution_stack_name = "64bit Amazon Linux 2 v4.3.7 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
   setting {
